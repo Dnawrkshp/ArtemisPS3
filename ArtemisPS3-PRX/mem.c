dcdc91b6d9951012fd812ce0628e512b8b942f84
Damian Parrino <dparrino@gmail.com>
2020-01-30 12:14:36 -0300

    Changes from vr5 plugin
    update by haxxen

--- ArtemisPS3-PRX/mem.c
+++ ArtemisPS3-PRX/mem.c
@@ -21,17 +21,30 @@ uint32_t get_heap(void)
 /***********************************************************************
 * create prx heap from memory container 0("game")
 ***********************************************************************/
-void create_heap(int32_t size)
+/* void create_heap(int32_t size)
 {
   mc_app = vsh_memory_container_by_id(0);
   sys_memory_allocate_from_container(KB(size * 64), mc_app, SYS_MEMORY_PAGE_SIZE_64K, &heap_mem);
   prx_heap = (uint32_t)heap_mem;
+} */
+int32_t create_heap(int32_t size)
+{
+  int32_t ret;
+
+  ret = sys_memory_allocate(MB(size), SYS_MEMORY_PAGE_SIZE_1M, &heap_mem);
+  if (ret != 0) {
+    // memory allocate error
+    heap_mem = 0;
+    return(-1);
+  }
+  prx_heap = (uint32_t)heap_mem;
+  return(0);
 }
 
 /***********************************************************************
 * 
 ***********************************************************************/
-void destroy_heap(void)
+/* void destroy_heap(void)
 {
 	if (prx_heap)
 	{
@@ -39,6 +52,15 @@ void destroy_heap(void)
 		prx_heap = 0;
 		mc_app = (sys_memory_container_t)-1;
 	}
+} */
+void destroy_heap(void)
+{
+  if (heap_mem) {
+    sys_memory_free((sys_addr_t)heap_mem);
+  }
+  heap_mem = 0;
+  prx_heap = 0;
+  mc_app = (sys_memory_container_t)-1;
 }
 
 /***********************************************************************
--- ArtemisPS3-PRX/mem.h
+++ ArtemisPS3-PRX/mem.h
@@ -6,10 +6,12 @@
 #define KB(x)      ((x)*1024)
 
 uint32_t get_heap(void);
-void create_heap(int32_t size);
+// void create_heap(int32_t size);
 void destroy_heap(void);
 void reset_heap(void);
 void *mem_alloc(uint32_t size);
+
+int32_t create_heap(int32_t size);
 
 
 #endif // __MEM_H__ 
